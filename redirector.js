583304391a333bb5c46c6b598a899a4ef2eac8cd
Steven Vondruska <vondruska@users.noreply.github.com>
2019-09-12 10:54:03 -0400

    Copy redirector.js from CloudFlare editor

---
+++ redirector.js
@@ -0,0 +1,33 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const redirects = new Map([
+  ["linkedin", "https://www.linkedin.com/in/stevenvondruka"],
+  ["github", "https://github.com/vondruska"]
+]);
+
+const defaultRedirect = "https://stevenvondruska.com";
+const responseCode = 302
+
+/**
+ * Fetch and log a request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  console.log('Got request', request);
+
+  const url = new URL(request.url);
+  console.log(url);
+
+  const lookup = url.pathname || "";
+  const trimmed = lookup.substring(1);
+
+  const location = redirects.get(trimmed);
+
+  if(location) {
+    return Response.redirect(location, responseCode);
+  } 
+
+  return Response.redirect(defaultRedirect, responseCode);
+}
