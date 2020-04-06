4283fb39e9f93b43cea906b11612c100e7587e90
Steven Vondruska <vondruska@users.noreply.github.com>
2020-04-05 20:57:06 -0400

    Adding Vondruska Internal CA
    Add redirect to Vondruska Internal CA root certificate

--- redirector.js
+++ redirector.js
@@ -5,7 +5,8 @@ addEventListener('fetch', event => {
 const redirects = new Map([
   ["linkedin", "https://www.linkedin.com/in/stevenvondruska"],
   ["github", "https://github.com/vondruska"],
-  ["ramhacks", "https://github.com/vondruska/ramhacks-2019-docker"]
+  ["ramhacks", "https://github.com/vondruska/ramhacks-2019-docker"],
+  ["ca", "https://gist.githubusercontent.com/vondruska/61cd4544fd7498f8dc47c8a5afb5b681/raw/19ddecc64c7221bcdd4a8839ebf223a610209daf/ca.pem"]
 ]);
 
 const defaultRedirect = "https://stevenvondruska.com";
